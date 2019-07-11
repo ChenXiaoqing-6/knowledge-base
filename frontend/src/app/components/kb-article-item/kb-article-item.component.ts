67a3a2e25920f745e16268a91f8ec76758f92aa2
Jelly Li <jelly.li@sap.com>
2019-07-11 16:30:49 +0800

    Implement UI to show article list and items

--- frontend/src/app/components/kb-article-item/kb-article-item.component.ts
+++ frontend/src/app/components/kb-article-item/kb-article-item.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { IArticle } from './../../models/IArticle';
+import { Component, OnInit, Input } from '@angular/core';
 
 @Component({
   selector: 'kb-article-item',
@@ -6,10 +7,35 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./kb-article-item.component.css']
 })
 export class KbArticleItemComponent implements OnInit {
-
+  @Input() article: IArticle;
   constructor() { }
 
   ngOnInit() {
+  }
+
+  copyArticleLink() {
+    if (document.queryCommandSupported && document.queryCommandSupported("copy")) {
+      let textarea = document.createElement("textarea");
+      textarea.textContent = this.article.link;
+      textarea.style.position = "fixed"; // Prevent scrolling to bottom of page in MS Edge.
+      document.body.appendChild(textarea);
+      textarea.select();
+      try {
+        document.execCommand("copy"); // Security exception may be thrown by some browsers.
+      } catch (ex) {
+        console.log(ex);
+      } finally {
+        document.body.removeChild(textarea);
+      }
+    }
+  }
+
+  onMore() {
+
+  }
+  
+  openArticleDetail() {
+
   }
 
 }
--- frontend/src/app/components/kb-article-list/kb-article-list.component.ts
+++ frontend/src/app/components/kb-article-list/kb-article-list.component.ts
@@ -1,12 +1,12 @@
-import { Component, OnInit } from '@angular/core';
-
+import { IArticle } from './../../models/IArticle';
+import { Component, OnInit, Input } from '@angular/core';
 @Component({
   selector: 'kb-article-list',
   templateUrl: './kb-article-list.component.html',
   styleUrls: ['./kb-article-list.component.css']
 })
 export class KbArticleListComponent implements OnInit {
-
+  @Input() articles: IArticle[];
   constructor() { }
 
   ngOnInit() {
