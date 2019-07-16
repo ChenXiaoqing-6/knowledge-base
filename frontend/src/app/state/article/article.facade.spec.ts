0d413e799ad8adef9efd584b1e88769a425e1626
Chen <mino.chen@sap.com>
2019-07-16 15:16:46 +0800

    add ut for article facade

---
+++ frontend/src/app/state/article/article.facade.spec.ts
@@ -0,0 +1,87 @@
+import { StoreMock } from '../mock/store.mock';
+import { KbViewFacade } from './article.facade';
+import { fakeAsync } from '@angular/core/testing';
+import { of, throwError } from 'rxjs';
+import { OpenArticle, LoadIFrameContentSuccess } from './article.actions';
+import { IArticle } from '../../models/IArticle';
+import { RenderType } from '../../models/RenderType.enum'
+
+describe('KbViewFacade facade test', () => {
+  function setup() {
+    const storeMock = new StoreMock();
+    const kbViewFacade = new KbViewFacade(
+      <any>storeMock
+    );
+
+    return {
+      kbViewFacade,
+      storeMock
+    };
+  }
+
+  describe(': openArticle ', () => {
+
+    it('shoudl dispatch to open article', () => {
+      const service = setup();
+      let article: IArticle = {
+        id: "testId",
+        provider: "testProvider",
+        title: "test title",
+        lastUpdated: new Date(),
+        score: 100,
+        link: "http://test.com",
+        renderType: RenderType.IFRAME,
+        renderValue: "test render",
+        views: 1,
+        author: "test author",
+      };
+
+      service.kbViewFacade.openArticle(article);
+
+      expect(service.storeMock.dispatch).toHaveBeenCalledTimes(1);
+      expect(service.storeMock.dispatch).toHaveBeenCalledWith(new OpenArticle(article));
+
+    });
+  });
+
+  describe(': isSearching ', () => {
+
+    it('should return true in the state', fakeAsync(() => {
+      const service = setup();
+      service.storeMock.pipe.and.returnValue(of(true));
+
+      service.kbViewFacade.isSearching().subscribe((_response: any) => {
+        expect(_response).toBeTruthy();
+        expect(service.storeMock.pipe).toHaveBeenCalledTimes(1);
+        expect(_response).toBe(true);
+      }, _error => {
+        expect(_error).toBeFalsy();
+      });
+    }));
+
+    it('should throw error -> observable error', fakeAsync(() => {
+      const service = setup();
+      const error = { error: 'TestError' };
+      service.storeMock.pipe.and.returnValue(throwError(error));
+
+      service.kbViewFacade.isSearching().subscribe((_response: any) => {
+        expect(_response).toBeFalsy();
+      }, _error => {
+        expect(_error).toBeTruthy();
+        expect(service.storeMock.pipe).toHaveBeenCalledTimes(1);
+        expect(_error).toBe(error);
+      });
+    }));
+  });
+
+  describe(': setContentLoadSuccess ', () => {
+
+    it('shoudl dispatch to open article', () => {
+      const service = setup();
+      service.kbViewFacade.setContentLoadSuccess();
+
+      expect(service.storeMock.dispatch).toHaveBeenCalledTimes(1);
+      expect(service.storeMock.dispatch).toHaveBeenCalledWith(new LoadIFrameContentSuccess());
+    });
+  });
+});
---
+++ frontend/src/app/state/mock/store.mock.ts
@@ -0,0 +1,4 @@
+export class StoreMock {
+  public dispatch: jasmine.Spy = jasmine.createSpy();
+  public pipe: jasmine.Spy = jasmine.createSpy();
+}
