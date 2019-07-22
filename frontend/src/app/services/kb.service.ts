0f52c35ee7c48f6534cd70cb3c395db2cd5fbff4
Mark <mark.zheng@sap.com>
2019-07-22 11:36:20 +0800

    move mock search function to mock-data.

--- frontend/src/app/services/kb.service.ts
+++ frontend/src/app/services/kb.service.ts
@@ -1,14 +1,12 @@
 import { Injectable } from '@angular/core';
-import { Observable, timer, throwError } from 'rxjs';
-import { mapTo, tap } from 'rxjs/operators';
-//import { map } from 'rxjs/operators';
-import { CollectionResponse } from '../models/IResponse';
+import { Observable } from 'rxjs';
 import { IArticle } from '../models/IArticle';
 import { ISearchOptions } from '../models/IRequestOptions';
+//import { map } from 'rxjs/operators';
+import { CollectionResponse } from '../models/IResponse';
 // import { AppConfig } from '../config/config';
 // import { BaseHttpClient } from '../services/http.client';
-
-import { MockArticleResponse, MockSearchResponse } from '../services/mock/mock-data';
+import { mockSearch } from '../services/mock/mock-data';
 
 @Injectable({
   providedIn: 'root'
@@ -21,43 +19,9 @@ export class KbService {
   //   return `${AppConfig.facadeBaseURL}/${endpoint}`;
   // }
 
-  private _mock(options: ISearchOptions): Observable<CollectionResponse<IArticle>> {
-
-    if (options.searchTerm.indexOf("Error") >= 0) {
-      return throwError(new Error('Fake error')); 
-    }
-
-    let data: Array<IArticle> = [];
-    let types: Array<string> = ["Java", "Angular", "Nodejs", "Javascript"];
-    for (let i = 1; i <= 100; i++) {
-      let tag = types[i % types.length];
-      data.push({ ...MockArticleResponse, id: 5000 + i, title: `mockArticle-${i}-tag[${tag}]` });
-    }
-
-    let typeData:Array<IArticle> = data.filter((article)=> {
-      return article.title.indexOf(options.searchTerm) >= 0;
-    });
-
-    let no = 0;
-    let typeDataPages: Array<Array<IArticle>> = [];
-    while(no < typeData.length) {
-      typeDataPages.push(typeData.slice(no, no+options.pagination.pageSize));
-      no += options.pagination.pageSize;
-    }
-    let ret = {
-      ...MockSearchResponse, 
-      data: (options.pagination.pageIndex <= typeDataPages.length) ? 
-        typeDataPages[options.pagination.pageIndex-1] : [], 
-      lastPage: typeDataPages.length || 1,  
-      totalObjectCount: typeData.length
-    };
-    console.log("request server: ", options);
-    return timer(1000).pipe(tap(()=>{console.log("done.");}),mapTo(ret));
-  }
-
   public searchArticles(options: ISearchOptions): Observable<CollectionResponse<IArticle>> {
 
-   return this._mock(options);
+   return mockSearch(options);
 
     // return this.http.get(this.getUrl("search")).pipe(
     //   map(res => res['data'])
--- frontend/src/app/services/mock/mock-data.ts
+++ frontend/src/app/services/mock/mock-data.ts
@@ -1,3 +1,9 @@
+import { ISearchOptions } from "../../models/IRequestOptions";
+import { Observable, timer, throwError } from "rxjs";
+import { CollectionResponse } from "../../models/IResponse";
+import { IArticle } from "../../models/IArticle";
+import { tap, mapTo } from "rxjs/operators";
+
 export const MockArticleResponse: any = {
     "id": "20001",
     "provider": "Mindtouch",
@@ -91,4 +97,38 @@ export const MockSearchResponse: any = {
     "currentPage": 1,
     "lastPage": 4,
     "totalObjectCount": 100
-};
+}
+
+
+export function mockSearch(options: ISearchOptions): Observable<CollectionResponse<IArticle>>  {
+    if (options.searchTerm.indexOf("Error") >= 0) {
+        return throwError(new Error('Fake error')); 
+      }
+  
+      let data: Array<IArticle> = [];
+      let types: Array<string> = ["Java", "Angular", "Nodejs", "Javascript"];
+      for (let i = 1; i <= 100; i++) {
+        let tag = types[i % types.length];
+        data.push({ ...MockArticleResponse, id: 5000 + i, title: `mockArticle-${i}-tag[${tag}]` });
+      }
+  
+      let typeData:Array<IArticle> = data.filter((article)=> {
+        return article.title.indexOf(options.searchTerm) >= 0;
+      });
+  
+      let no = 0;
+      let typeDataPages: Array<Array<IArticle>> = [];
+      while(no < typeData.length) {
+        typeDataPages.push(typeData.slice(no, no+options.pagination.pageSize));
+        no += options.pagination.pageSize;
+      }
+      let ret = {
+        ...MockSearchResponse, 
+        data: (options.pagination.pageIndex <= typeDataPages.length) ? 
+          typeDataPages[options.pagination.pageIndex-1] : [], 
+        lastPage: typeDataPages.length || 1,  
+        totalObjectCount: typeData.length
+      };
+      console.log("request server: ", options);
+      return timer(1000).pipe(tap(()=>{console.log("done.");}),mapTo(ret));
+}
