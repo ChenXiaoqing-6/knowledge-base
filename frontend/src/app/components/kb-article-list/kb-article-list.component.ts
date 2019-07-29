1919f0e8e4e6be4379d7c644ac2bcd64c7f06dbe
I506612 <zihan.wang@sap.com>
2019-07-29 17:19:06 +0800

    unify UI of Article/Suggested/Linked-List

--- frontend/src/app/components/kb-article-list/kb-article-list.component.ts
+++ frontend/src/app/components/kb-article-list/kb-article-list.component.ts
@@ -7,6 +7,7 @@ import { Component, OnInit, Input } from '@angular/core';
 })
 export class KbArticleListComponent implements OnInit {
   @Input() articles: IArticle[];
+  type: String = "ArticleList";
   constructor() { }
 
   ngOnInit() {
